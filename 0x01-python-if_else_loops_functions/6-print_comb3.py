e329240f4a97da2fd5d5fdba1e0e40ad77619869
Mamo Fideno <mamofideno@gmail.com>
2022-05-31 07:57:15 -0700

    cr

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for ii in range(0, 9):
     for i in range(ii , 10):
-        print("{}{} ".format(ii, i), end="")
-print("")
+        if i == 8 and ii == 9:
+            print("{}{}".format(ii,i))
+        print("{}{}, ".format(ii, i), end="")

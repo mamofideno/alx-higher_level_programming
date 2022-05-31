6d5dd9ccb4e8a681806fc4e211750b6111586e07
Mamo Fideno <mamofideno@gmail.com>
2022-05-31 07:50:04 -0700

    sb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,4 @@
 for i in range(0, 100):
     if i == 99:
         print("{}".format(i))
-    print("{:02d} ".format(i, i))
+    print("{:02d} ".format(i, i), end="")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for ii in range(0, 9):
+    for i in range(ii , 10):
+        print("{}{} ".format(ii, i), end="")
+print("")

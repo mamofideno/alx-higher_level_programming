f96dce072efe59eb30b2dc760958d8050965d3b6
Mamo Fideno <mamofideno@gmail.com>
2022-06-05 23:43:13 -0700

    modify

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+
+def print_list_integer(my_list = []):
     for a in my_list:
-        print("{}\n".format(a))
+        print("{}".format(a))

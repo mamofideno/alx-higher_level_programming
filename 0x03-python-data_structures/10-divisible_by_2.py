040a19287ce993cf919a84159ebad6ddcf7e0834
Mamo Fideno <mamofideno@gmail.com>
2022-06-07 05:03:46 -0700

    ds

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list = []):
-    lis = []
+def divisible_by_2(my_list=[]):
+    lis= []
     for i in my_list:
         lis.append((i % 2) == 0)
     return lis

178c1077ba78c08d163955e1b0229d5b54fcd044
Mamo Fideno <mamofideno@gmail.com>
2022-06-08 05:07:10 -0700

    ds

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(0,len(my_list)):
+    for i in range(0, len(my_list)):
         if search == my_list[i]:
-            my_list[i]= replace
+            my_list[i] = replace
     return (my_list)

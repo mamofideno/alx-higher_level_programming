5dfb62189e54ed34ec94f0d9673fdb94b2407480
Mamo Fideno <mamofideno@gmail.com>
2022-06-20 05:04:14 -0700

    updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
-        """
+    """
     divides two integers and prints the result
     catches divide by zero exception
     """
@@ -12,4 +11,4 @@ def safe_print_division(a, b):
          res = None
      finally:
          print("Inside result: {}".format(res))
-         return res
+     return res

b70753dc2abb7872fcbeb5b482752aba31287e0d
Mamo Fideno <mamofideno@gmail.com>
2022-06-20 04:26:32 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
+    printed = 0
+    for i in range(0, x):
         try:
-            print(my_list[i])
+            print("{}".format(my_list[i]))
+            printed++
         except:
-            print()
+            continue
+    return printed    
